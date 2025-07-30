a08593f48c876b32c7177553c99a753225d906d3
aninhah25 <136709743+aninhah25@users.noreply.github.com>
2025-07-30 13:36:22 -0300

    ..

---
+++ scrip.js
@@ -0,0 +1,6 @@
+function insirirNome(){
+    let nomeUsuario =prompt("Qual é o seu nome")
+    let elemento =document.querySelector ("#nome-usuario");
+    console.log(elemento);
+    elemento.textContent = nomeUsuario;
+}
